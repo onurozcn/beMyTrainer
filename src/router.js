c14b933ac1af60456c1abb5ebca10cef90cfabbd
Onur Ozcan <ozcan-onur@hotmail.com>
2022-10-26 22:13:05 +0200

    Import pages, and set components

--- src/router.js
+++ src/router.js
@@ -1,17 +1,29 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
+import TrainerDetail from './pages/trainers/TrainerDetail.vue'
+import TrainersList from './pages/trainers/TrainersList.vue'
+import TrainerRegistration from './pages/trainers/TrainerRegistration.vue'
+import ContactTrainer from './pages/contact/ContactTrainer.vue'
+import RequestsRecived from './pages/requsts/RequstsRecived.vue'
+import NotFound from './pages/NotFound.vue'
+
+
+
 const router = createRouter({
   history: createWebHistory(),
-    routes: [
-    
+  routes: [
     { path: '/', component: '/trainers' },
-    { path: '/trainers', component: null },
-    {path: '/trainers/:id', component: null, children: [
-              { path: 'contact', component: null}, // /coaches/coachID/contact
-          ]},
-    { path: '/register', component: null },
-    { path: '/requests', component: null },
-    {path: '/:notFound(.*), component: null'}
+    { path: '/trainers', component: TrainersList },
+    {
+      path: '/trainers/:id',
+      component: TrainerDetail,
+      children: [
+        { path: 'contact', component: ContactTrainer }, // /coaches/coachID/contact
+      ],
+    },
+    { path: '/register', component: TrainerRegistration },
+    { path: '/requests', component: RequestsRecived },
+    { path: '/:notFound(.*)', component: NotFound },
   ],
 });
 

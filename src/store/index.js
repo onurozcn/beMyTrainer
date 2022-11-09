d543d05590ec00ec8a24445598bae26a604d30f3
Onur Ozcan <ozcan-onur@hotmail.com>
2022-11-09 07:45:59 +0100

    Import modules

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,22 @@
 import { createStore } from 'vuex'
 
 import trainersModule from './modules/trainers/index.js'
+import requestsModule from './modules/requests/index.js'
 
 const store = createStore({
     modules: {
-        trainers: trainersModule
-
+        trainers: trainersModule,
+        requests: requestsModule,
+    },
+    state() {
+        return {
+            userID: 'c3'
+        }
+    },
+    getters: {
+        userId(state) {
+            return state.userID
+        }
     }
 })
 

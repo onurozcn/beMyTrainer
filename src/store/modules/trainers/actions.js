a81b76ccab04ddee63dfbb20d0855eafc284b975
Onur Ozcan <ozcan-onur@hotmail.com>
2022-11-09 08:23:20 +0100

    Store trainer data in Firebase. Create PUT Http request

--- src/store/modules/trainers/actions.js
+++ src/store/modules/trainers/actions.js
@@ -1,14 +1,31 @@
 export default {
-    registerTrainer(context, data) {
+    async registerTrainer(context, data) {
+        const userId = context.rootGetters.userId //dummy
         const trainerData = {
-            id: context.rootGetters.userId, //dummy
             firstName: data.first,
             lastName: data.last,
             description: data.desc,
             hourlyRate: data.rate,
             areas: data.areas
         }
-        context.commit('registerTrainer', trainerData)
+        const response = await fetch(
+            `https://be-my-trainer-default-rtdb.europe-west1.firebasedatabase.app/trainers/${userId}.json`,
+            {
+                method: 'PUT',
+                body: JSON.stringify(trainerData),
+            }
+        );
+
+        // const responseData = await response.json()
+
+        if (!response.ok) {
+            // error ...
+        }
+
+        context.commit('registerTrainer', {
+            ...trainerData,
+            id: userId,
+        })
     }
 }
 

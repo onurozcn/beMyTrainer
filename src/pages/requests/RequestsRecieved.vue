0581586c48188697f334089a75b30d4494b0a310
Onur Ozcan <ozcan-onur@hotmail.com>
2022-11-11 13:37:11 +0100

    Show error dialog. Add loading spinner for requests

--- src/pages/requests/RequestsRecieved.vue
+++ src/pages/requests/RequestsRecieved.vue
@@ -1,10 +1,14 @@
 <template>
+<base-dialog :show="!!error" title="An error occured!" @close= "handleError">
+    <p>{{error}}</p>
+  </base-dialog>
     <section>
         <base-card>
             <header>
                 <h2>Request Recieved</h2>
             </header>
-            <ul v-if="hasRequests">
+            <base-spinner v-if="isLoading"></base-spinner>
+            <ul v-else-if="hasRequests && !isLoading">
                 <request-item v-for="req in receivedRequest" :key="req.id" :email="req.userEmail" :message="req.message"></request-item>
             </ul>
             <h3 v-else>You haven't received any requests yet!</h3>
@@ -19,12 +23,34 @@ export default {
     components: {
         RequestItem,
     },
+    data(){
+       return{
+        isLoading: false,
+        error: null
+       } 
+    },
     computed: {
         receivedRequest(){
            return this.$store.getters['requests/requests']
         },
         hasRequests(){
             return this.$store.getters['requests/hasRequests']
+        },
+
+    },
+    methods: {
+        async loadRequests() {
+            this.isLoading = true;
+            try{
+              await this.$store.dispatch('requests/fetchRequests')
+            }
+            catch(error){
+                this.error= error.message || 'Something failed!'
+            }
+            this.isLoading = false
+        },
+        handleError(){
+            this.error = null
         }
     }
 }
